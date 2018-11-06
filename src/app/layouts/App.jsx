a2905ac7db4cec264f8560065ca783b987f06bf3
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-06 04:37:44 +0000

    Setup npm-shrinkwrap

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1></h1>
+      <div>
+        <h1>Re-vents</h1>
       </div>
     )
   }
